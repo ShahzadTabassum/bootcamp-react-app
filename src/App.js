7b9fd96c885e0ecd2ea32b2f4bd7bfb73430b176
ShahzadTabassum <www.shahzadtabassum786@gmail.com>
2020-09-21 20:30:26 +0500

    More Updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World for all Pakistan</h1>
+      <h1>Hello World For All Pakistan</h1>
     </div>
   );
 }
